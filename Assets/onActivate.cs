ecf7156e0fc5b9d242a3508ea4306edca7026499
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-11-27 01:31:09 +0800

    feat: add drag object and weight detector

---
+++ Assets/onActivate.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onActivate : MonoBehaviour
+{
+    bool pressure = false;
+    GameObject washingHatch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressure)
+        {
+            GameObject.Find("WeightDetector").GetComponent<Renderer>().material.color = new Color(100,0,0);
+        }
+        else 
+        {
+            GameObject.Find("WeightDetector").GetComponent<Renderer>().material.color = new Color(0,0,100);
+        }
+    }
+
+    public void changeRed()
+    {
+        pressure = true;
+    }
+    
+    public void changeBlue()
+    {
+        pressure = false;
+    }
+}

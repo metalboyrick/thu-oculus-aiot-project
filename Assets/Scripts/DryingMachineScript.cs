5cb9c63c34524ebafcafe81e72bae5685e640436
Rick <rsulisthio@gmail.com>
2021-12-16 15:47:41 +0700

    feat:add open close voice command

--- Assets/Scripts/DryingMachineScript.cs
+++ Assets/Scripts/DryingMachineScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WashMachineScript : MonoBehaviour
+public class DryingMachineScript : MonoBehaviour
 {
     bool isOpen = false;
     GameObject washingHatch;
@@ -11,31 +11,32 @@ public class WashMachineScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        washingHatch = GameObject.Find("WashingHatch");
+        washingHatch = GameObject.Find("DryingHatch");
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    public void OpenMachine()
-    {
-        if (!isOpen)
+        if(isOpen)
         {
             Quaternion target = Quaternion.Euler(0, 0, -90);
             washingHatch.transform.localRotation = target;
         }
-        isOpen = true;
-    }
-    
-    public void CloseMachine()
-    {
-        if (isOpen)
+        else 
         {
             Quaternion target = Quaternion.Euler(0, 0, 0);
             washingHatch.transform.localRotation = target;
         }
+    }
+
+    public void OpenMachine()
+    {
+        isOpen = true;
+        Debug.Log("Voice command detected");
+    }
+    
+    public void CloseMachine()
+    {
         isOpen = false;
     }
 }
